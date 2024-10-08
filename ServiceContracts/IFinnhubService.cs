b690b4fff72076eb778573a25901f62a61ebbbe9
Ahmed Usama <ausama@innotech.com.eg>
2024-10-08 14:36:22 +0300

    Add project files.

---
+++ ServiceContracts/IFinnhubService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceContracts
+{
+ public interface IFinnhubService
+ {
+  Dictionary<string, object>? GetCompanyProfile(string stockSymbol);
+
+
+  Dictionary<string, object>? GetStockPriceQuote(string stockSymbol);
+ }
+}
---
+++ StockMarketSolution/Controllers/TradeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using ServiceContracts;
+using StockMarketSolution.Models;
+
+namespace StockMarketSolution.Controllers
+{
+ [Route("[controller]")]
+ public class TradeController : Controller
+ {
+  private readonly TradingOptions _tradingOptions;
+  private readonly IFinnhubService _finnhubService;
+  private readonly IConfiguration _configuration;
+
+
+
+  public TradeController(IOptions<TradingOptions> tradingOptions, IFinnhubService finnhubService, IConfiguration configuration)
+  {
+   _tradingOptions = tradingOptions.Value;
+   _finnhubService = finnhubService;
+   _configuration = configuration;
+  }
+
+
+  [Route("/")]
+  [Route("[action]")]
+  [Route("~/[controller]")]
+  public IActionResult Index()
+  {
+   if (string.IsNullOrEmpty(_tradingOptions.DefaultStockSymbol))
+    _tradingOptions.DefaultStockSymbol = "MSFT";
+
+
+   Dictionary<string, object>? companyProfileDictionary = _finnhubService.GetCompanyProfile(_tradingOptions.DefaultStockSymbol);
+
+   Dictionary<string, object>? stockQuoteDictionary = _finnhubService.GetStockPriceQuote(_tradingOptions.DefaultStockSymbol);
+
+
+   StockTrade stockTrade = new StockTrade() { StockSymbol = _tradingOptions.DefaultStockSymbol };
+
+   if (companyProfileDictionary != null && stockQuoteDictionary != null)
+   {
+    stockTrade = new StockTrade() { StockSymbol = Convert.ToString(companyProfileDictionary["ticker"]), StockName = Convert.ToString(companyProfileDictionary["name"]), Price = Convert.ToDouble(stockQuoteDictionary["c"].ToString()) };
+   }
+
+   ViewBag.FinnhubToken = _configuration["FinnhubToken"];
+
+   return View(stockTrade);
+  }
+ }
+}
+
+
